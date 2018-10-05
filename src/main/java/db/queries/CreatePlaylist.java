b91b5ad6d5d91e078fc1c449de6fbed9e75dc5a8
Montgomery <montgomerywatts@yahoo.com>
2018-10-05 14:01:15 -0400

    CreatePlaylist.java: 	Removed redundant queries to the collabs collection.

--- src/main/java/db/queries/CreatePlaylist.java
+++ src/main/java/db/queries/CreatePlaylist.java
@@ -34,18 +34,11 @@ public class CreatePlaylist {
             }
         }
 
-        //Get songs the artist has featured on
-        List<Document> artistFeatured = collabCollection.find( new Document("collabs", artistName) )
-                .into(new ArrayList<>());
-        for (Document d: artistFeatured){
-            String name = d.getString("_id");
-            artistNames.add(name);
-        }
-
         //Add all artists the given artist has worked with
         artistNames.addAll( getArtistCollabs(collabCollection, artistName) );
 
         for (String name: artistNames){
+            System.out.println(name);
             songs.addAll( getArtistSongs(songsCollection, name) );
         }
 
@@ -63,4 +56,45 @@ public class CreatePlaylist {
         }
     }
 
+    public static List<String> getTrackUris(String artist){
+        int duration = 3600;
+        MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("music");
+        MongoCollection<Document> artistCollection = db.getCollection("artists");
+        MongoCollection<Document> songsCollection = db.getCollection("songs");
+        MongoCollection<Document> collabCollection = db.getCollection("collabs");
+
+        Document artistDoc = getArtistDoc(artistCollection, artist);
+        String artistName = artistDoc.getString("_id");
+        Set<Document> songs = getArtistSongs(songsCollection, artistName);
+        List<String> artistGenres = (ArrayList<String>) artistDoc.get("genres");
+
+        //Get all artists with related genres
+        HashSet<String> artistNames = new HashSet<>();
+        for (String genre: artistGenres) {
+            for (Document artistDocs : artistCollection.find(new Document("genres", genre))){
+                String name = artistDocs.getString("_id");
+                artistNames.add(name);
+            }
+        }
+
+        //Add all artists the given artist has worked with
+        artistNames.addAll( getArtistCollabs(collabCollection, artistName) );
+
+        for (String name: artistNames){
+            songs.addAll( getArtistSongs(songsCollection, name) );
+        }
+
+        List<Document> possibleSongs = new ArrayList<>(songs);
+        List<String> playlistURIs = new ArrayList<>();
+        while (duration > 180){
+            Document d = possibleSongs.remove(Math.abs(new Random().nextInt()) % possibleSongs.size());
+            int songDuration = d.getInteger("duration");
+            if (songDuration <= duration)
+                playlistURIs.add( d.getString("_id") );
+            duration -= songDuration;
+        }
+
+        return playlistURIs;
+    }
 }
