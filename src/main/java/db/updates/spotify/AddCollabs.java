0f0b3fb555258a8c9292228c24efdcf4b5af7a33
Montgomery <montgomerywatts@yahoo.com>
2018-10-05 01:27:07 -0400

    AddCollabs.java: 	Refactored to properly establish relationships between artists.

--- src/main/java/db/updates/spotify/AddCollabs.java
+++ src/main/java/db/updates/spotify/AddCollabs.java
@@ -1,11 +1,13 @@
 package db.updates.spotify;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static com.mongodb.client.model.Filters.*;
@@ -25,19 +27,58 @@ public class AddCollabs {
         MongoCollection<Document> artists = db.getCollection("artists");
         MongoCollection<Document> collabs = db.getCollection("collabs");
 
+
         for(Document artistDoc: artists.find()){
-            String artistName = artistDoc.getString("_id");
-            ArrayList<String> featuredArtists = new ArrayList<>();
-            for (Document songDoc: songs.find( and(eq("artist", artistName), exists("featured") )) ){
-                for (String featured: (List<String>) songDoc.get("featured")){
-                    if(!featuredArtists.contains(featured)){ //Assuming few elements so linear search isn't costly
+            try {
+                String artistName = artistDoc.getString("_id");
+                HashSet<String> featuredArtists = new HashSet<>();
+
+                //For every song Document that is by the artist and has other artists featured on it
+                for (Document songDoc : songs.find(and(eq("artist", artistName), exists("featured")))) {
+                    for (String featured : (List<String>) songDoc.get("featured")) {
                         featuredArtists.add(featured);
+
+
+                        //If an artist has worked with other this should be shown in both of their docs
+                        Document featuredDoc = collabs.find(eq("_id", featured)).first();
+
+                        //If the other artist already has a document
+                        if (featuredDoc != null) {
+                            HashSet<String> otherFeaturedArtists = new HashSet<>((ArrayList<String>) featuredDoc.get("collabs"));
+                            otherFeaturedArtists.add(artistName);
+                            featuredDoc.put("collabs", otherFeaturedArtists);
+                            collabs.replaceOne(eq("_id", featured), featuredDoc);
+                        }
+                        else{
+                            HashSet<String> singleton = new HashSet<>();
+                            singleton.add(artistName);
+                            featuredDoc = new Document("_id", featured)
+                                .append("collabs", singleton);
+                            collabs.insertOne(featuredDoc);
+                        }
+
+
                     }
                 }
+
+                Document collabDoc = collabs.find( eq("_id", artistName)).first();
+
+                //If the current artist's document has already been created by the insertion of another artist's doc
+                if(collabDoc != null){
+                    HashSet<String> existingFeatures = new HashSet<>((ArrayList<String>) collabDoc.get("collabs"));
+                    featuredArtists.addAll( existingFeatures );
+                    collabDoc.put("collabs", featuredArtists);
+                    collabs.replaceOne( eq("_id", artistName), collabDoc);
+                }
+                else{
+                    collabDoc = new Document("_id", artistName)
+                            .append("collabs", featuredArtists);
+                    collabs.insertOne(collabDoc);
+                }
+
+            } catch (MongoWriteException mwe){
+                mwe.printStackTrace();
             }
-            Document collabDoc = new Document("_id", artistName)
-                    .append("collabs", featuredArtists);
-            collabs.insertOne(collabDoc);
         }
     }
 }
