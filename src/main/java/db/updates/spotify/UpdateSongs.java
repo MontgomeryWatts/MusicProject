b3152880a24f77f448b1bda1dcf3198796ef8a4e
Montgomery <montgomerywatts@yahoo.com>
2018-10-06 13:47:54 -0400

    UpdateSongs.java: 	Program that queries Spotify to see if there are any new songs from all of the artists in the database. May potentially be improved by querying the artist's albums locally then comparing between albums retrieved from Spotify.

---
+++ src/main/java/db/updates/spotify/UpdateSongs.java
@@ -0,0 +1,27 @@
+package db.updates.spotify;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * This file goes through the entire artists collection and checks if there are any new songs from each of the
+ * artists and attempts to add it to the songs collection.
+ */
+
+public class UpdateSongs {
+    public static void main(String[] args) {
+        MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("music");
+        MongoCollection<Document> songsCollection = db.getCollection("songs");
+        MongoCollection<Document> artistsCollection = db.getCollection("artists");
+
+        for(Document artistDoc: artistsCollection.find() ){
+            String artistName = artistDoc.getString("_id");
+            System.out.println("Attempting to add new songs for " + artistName);
+            SpotifyQueries.addSongs(songsCollection, artistName);
+
+        }
+    }
+}
